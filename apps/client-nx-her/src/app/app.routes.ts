83ddb18a48e5b6ff0c6e2775209bc9b8d6a157e5
Matthijs Feringa <mt.feringa@student.avans.nl>
2023-12-04 23:57:00 +0100

    setting over stuff from first application to nx project

--- apps/client-nx-her/src/app/app.routes.ts
+++ apps/client-nx-her/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Route } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { ParkListComponent } from './features/themepark/themepark-list/themepark-list.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'parks',
+    component: ParkListComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- libs/themeparks/features/src/index.ts
+++ libs/themeparks/features/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/park/features.module';
+export * from './lib/park/park.service';
