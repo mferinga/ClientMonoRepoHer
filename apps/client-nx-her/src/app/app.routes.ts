77a83e7a8fd5e9d5c4cc6f6939044efd903366ac
Matthijs Feringa <mt.feringa@student.avans.nl>
2023-12-11 23:12:19 +0100

    working on detail page

--- apps/client-nx-her/src/app/app.routes.ts
+++ apps/client-nx-her/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Route } from '@angular/router';
 import { HomeComponent } from 'libs/themeparks/features/src/lib/home/home.component';
+import { ParkDetailComponent } from 'libs/themeparks/features/src/lib/park/park-detail/park-detail.component';
 import { ParkListComponent } from 'libs/themeparks/features/src/lib/park/park-list/park-list.component';
 
 export const appRoutes: Route[] = [
@@ -19,4 +20,12 @@ export const appRoutes: Route[] = [
     path: 'parks',
     component: ParkListComponent,
   },
+  {
+    path: 'parks/:id',
+    component: ParkDetailComponent,
+  },
+  // {
+  //   path: 'parks/:id/edit',
+  //   component: ,
+  // }
 ];
--- libs/themeparks/features/src/lib/park/park-detail/park-detail.component.ts
+++ libs/themeparks/features/src/lib/park/park-detail/park-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ParkService } from '../park.service';
 import { IPark } from '@client-nx-her/shared/api';
 import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'client-nx-her-park-detail',
@@ -11,13 +12,28 @@ import { Subscription } from 'rxjs';
 export class ParkDetailComponent implements OnInit, OnDestroy {
   park: IPark | null = null;
   subscription: Subscription | undefined = undefined;
+  id: number | undefined;
 
-  constructor(private parkService: ParkService) {}
+  constructor(
+    private parkService: ParkService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.subscription = this.parkService.read('1').subscribe((result) => {
-      console.log(`results: ${result}`);
-      this.park = result;
+    this.route.paramMap.subscribe((params) => {
+      this.id = Number(params.get('id'));
+      if (this.id) {
+        this.subscription = this.parkService
+          .list(this.id)
+          .subscribe((results) => {
+            console.log(`results: ${results}`);
+            if (results != null) {
+              this.park = results[0];
+            }
+          });
+      } else {
+        console.log('Error: id is undefined');
+      }
     });
   }
 
