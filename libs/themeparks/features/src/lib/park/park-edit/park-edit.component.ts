5d574b94c6d2575a554eba2e610ff055128bb16c
Matthijs Feringa <mt.feringa@student.avans.nl>
2023-12-12 18:49:19 +0100

    made park edit component in main

---
+++ libs/themeparks/features/src/lib/park/park-edit/park-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'client-nx-her-park-edit',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './park-edit.component.html',
+  styleUrl: './park-edit.component.css',
+})
+export class ParkEditComponent {}
